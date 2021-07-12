d0e06bca99744756ea66ba212fb3b228a0a8aba1
unknown <odimaa.horsfall@gmail.com>
2021-07-12 18:39:42 +0100

    more changes

--- js/main.js
+++ js/main.js
@@ -17,29 +17,7 @@
   });
 
   
-
-  var form = document.getElementById("my-form");
-
-  async function handleSubmit(event) {
-	  event.preventDefault();
-	  var status = document.getElementById("my-form-status");
-	  var data = new FormData(event.target);
-	  fetch(event.target.action, {
-		  method: form.method,
-		  body: data,
-		  headers: {
-			  'Accept': 'application/json'
-		  }
-	  }).then(response => {
-		  status.innerHTML = "Message Received! Thank you";
-		  form.reset()
-	  }).catch(error => {
-		  status.innerHTML = "Sorry! There was a problem submitting your form"
-	  });
-  }
-  form.addEventListener("submit", handleSubmit)
-
-
+ 
 
 	var fullHeight = function() {
 
